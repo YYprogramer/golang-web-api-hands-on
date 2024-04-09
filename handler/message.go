6b6fc933b57b227a3e1284e14b396696690e9bc9
YYprogramer <131517182+YYprogramer@users.noreply.github.com>
2024-04-10 04:57:19 +0900

    handler/message.goを修正

--- handler/message.go
+++ handler/message.go
@@ -1,10 +1,12 @@
 package handler
 
 import (
+	"net/http"
+
 	"golang-web-api-hands-on/handler/response"
 	"golang-web-api-hands-on/usecase"
 
-	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/render"
 )
 
 type Message interface {
@@ -15,7 +17,7 @@ type messageHandler struct {
 	useCase usecase.Message
 }
 
-func NewMessageHandler(useCase usecase.Message) Message {
+func NewMessage(useCase usecase.Message) Message {
 	return &messageHandler{
 		useCase: useCase,
 	}
@@ -24,25 +26,16 @@ func NewMessageHandler(useCase usecase.Message) Message {
 func (m *messageHandler) Get(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
-	messageID := chi.URLParam(r, "messageID")
+	message := m.useCase.Get(ctx)
 
-	if messageID == "" {
+	if message == "" {
 		render.Status(r, http.StatusNotFound)
 		render.JSON(w, r, response.ErrNotFound)
-	}
-
-	result, err := b.useCase.Get(ctx, messageID)
-	if err != nil {
-		if err.Error() == "could not find message" {
-			render.Status(r, http.StatusNotFound)
-			render.JSON(w, r, response.ErrNotFound)
-			return
-		}
-		render.Status(r, http.StatusInternalServerError)
-		render.JSON(w, r, response.ErrSystemError)
 		return
 	}
 
 	render.Status(r, http.StatusOK)
-	render.JSON(w, r, response.NewMessage(result))
+	render.JSON(w, r, map[string]string{
+		"message": message,
+	})
 }
